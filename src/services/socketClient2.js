22d0a6f29608cd0aa5ef2d966322886ea178cf28
weixiaoyi <2278095567@qq.com>
2018-11-14 23:45:46 +0800

    websocket改造

--- src/services/socketClient2.js
+++ src/services/socketClient2.js
@@ -3,20 +3,42 @@ import { forkJoin, from, of, Subject, } from 'rxjs'
 import { map, race, retry, catchError, tap, take, share, debounceTime, takeWhile } from 'rxjs/operators'
 
 class Ws {
-  constructor(url) {
+  constructor({ url, flush = true }) {
+    this.subscribeRecords = []
+    this.promises = []
     this.ws$ = new Subject()
     this.ws = new WebSocket(url)
     this.ws.onopen = () => {
+      if (this.promises.length) {
+        this.promises.forEach(item => this.send(item))
+        this.promises = []
+      }
     }
     this.ws.onmessage = (e) => {
-      this.ws$.next(formatJson(e.data))
+      this.ws$.next([e, formatJson(e.data)])
     }
     this.ws.onclose = (e) => {
-
+      console.log(`${url}连接关闭...`, e)
+      this.repeatConnect(e)
     }
     this.ws.onerror = (e) => {
-
+      console.log(`${url}连接错误`, e)
+      // 连接错误自动会促发连接关闭，这里不需要再次连接
     }
+  }
+
+  hasConnected = () => {
+    return this.ws.readyState === 1
+  }
+
+  send = (obj = {}) => {
+    this.subscribeRecords.push(obj)
+    if (!this.hasConnected()) {
+      this.promises.push(obj)
+    } else {
+      this.ws.send(JSON.stringify(obj))
+    }
+    return this.ws$
   }
 
 
@@ -33,4 +55,4 @@ class Ws {
   }
 }
 
-export default new Ws('ws://api.weixiaoyi/ws')
+export default new Ws({ url: 'ws://api.weixiaoyi/ws' })
